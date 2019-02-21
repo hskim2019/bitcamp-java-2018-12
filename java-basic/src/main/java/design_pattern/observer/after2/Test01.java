04819e54e2718038b0eefa138da65ec3ef6bf115
saemihan <ansaemih@gmail.com>
2019-02-21 19:31:15 +0900

    .

--- java-basic/src/main/java/design_pattern/observer/after2/Test01.java
+++ java-basic/src/main/java/design_pattern/observer/after2/Test01.java
@@ -18,11 +18,11 @@ public class Test01 {
     
     System.out.println("--------------------------------------------");
     
-    car.addObserver(new SafeBeltCarObserver());
-    car.addObserver(new EngineOilCarObserver());
+//    car.addObserver(new SafeBeltCarObserver());
+//    car.addObserver(new EngineOilCarObserver());
     car.addObserver(new BreakCarObserver());
-    car.addObserver(new LightOffCarObserver());
-    car.addObserver(new SunRoofCloseCarObserver());
+//    car.addObserver(new LightOffCarObserver());
+//    car.addObserver(new SunRoofCloseCarObserver());
     
     car.start();
     car.run();
